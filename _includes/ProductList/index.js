ed3e61d29a6852af7b69c22a3fba71fb02c3d0ff
komaschuk <d.komaschuk@gmail.com>
2016-09-11 13:31:45 +0300

    add product-list

---
+++ _includes/ProductList/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var productItem = {
+        origin: 'bottom',
+        delay: 400,
+        distance: '200px',
+        easing: 'ease',
+        scale: 1,
+        mobile: false,
+        rotate   : { z: 10 },
+        // reset: false
+    };
+
+    window.sr = ScrollReveal({ reset: true });
+
+    sr.reveal('.js-product-list__item', productItem);
+});
